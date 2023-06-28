68246d43fa7047024ae63c7b0156c957d105f1e3
bielzinho <devbielzinho@gmail.com>
2023-06-28 08:36:26 -0300

    ok

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getAnimations('myVideo');
+
+    if (performance.navigation.type === 1) {
+        video.play
+    }
+
+    video.addEventListener('canplay', function() {
+        video.play();
+    });
+});
+
 var audio = document.getElementById('myAudio');
 
-audio.volume = 0.2;
+audio.volume = 0.03;
---
+++ squarecloud.app
@@ -0,0 +1,6 @@
+DISPLAY_NAME=Site muito legal
+MAIN=index.html
+SUBDOMAIN=sitelindo
+START=npm run start-build
+MEMORY=512
+VERSION=recommended
